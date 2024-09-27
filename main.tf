f528a3b03645d24ef916ddbc753e901c956bbc67
Matheus Tadeu <lmmatheustadeu@gmail.com>
2024-09-27 07:52:20 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+variable "environment" {
+  description = "The environment to deploy (dev or main)"
+  type        = string
+}
+
+variable "db_username" {
+  description = "The database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "The database password"
+  type        = string
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "grupo51" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t2.micro"
+  identifier           = "grupo51-${var.environment}"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
