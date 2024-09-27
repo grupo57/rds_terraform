56dbc8073643b3899e5dcd94da92ac47617b3d54
matheustadeu <matheustadeu@movida.com.br>
2024-09-27 13:58:25 -0300

    Ajustando ci

--- main.tf
+++ main.tf
@@ -20,11 +20,11 @@ provider "aws" {
 resource "aws_db_instance" "grupo51" {
   allocated_storage    = 20
   engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t2.micro"
+  engine_version       = "5.7 "
+  instance_class       = "db.t3.micro"
   identifier           = "grupo51-${var.environment}"
   username             = var.db_username
   password             = var.db_password
-  parameter_group_name = "default.mysql8.0"
+  parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
